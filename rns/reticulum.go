33bf6a61e97ce9d4e2a17867b4a411cd5f89de98
s.vanichkin <github@macflash.ru>
2025-12-31 12:26:38 +0300

    Windows autobuild fixes

--- rns/reticulum.go
+++ rns/reticulum.go
@@ -3023,19 +3023,7 @@ func (r *Reticulum) GetInterfaceStats() map[string]any {
 }
 
 func processRSSBytes() any {
-	var ru syscall.Rusage
-	if err := syscall.Getrusage(syscall.RUSAGE_SELF, &ru); err != nil {
-		return nil
-	}
-	// Linux reports kilobytes, Darwin reports bytes.
-	v := ru.Maxrss
-	if runtime.GOOS == "linux" {
-		v *= 1024
-	}
-	if v <= 0 {
-		return nil
-	}
-	return v
+	return processRSSBytesPlatform()
 }
 
 func (r *Reticulum) GetPathTable(maxHops int) []map[string]any {
